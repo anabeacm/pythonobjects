c4ed5b2efb2ab3d81b0a5c272e3b97f9b280fdc6
Ana <ana.menezes@alu.ufc.br>
2026-07-21 14:41:21 -0300

    Aprendendo conceitos de encapsulamento básico e condicionamento dos métodos

--- Questões/quest4.py
+++ Questões/quest4.py
@@ -11,3 +11,40 @@ Depois:
     Crie outro para mostrar os dados do produto.
     Atualize o preço de dois produtos.
 '''
+
+class Produto:
+    def __init__(self, nome, preco, qtd):
+
+        self.nome = nome
+        self.preco = preco
+        self.qtd = qtd
+
+    def atPreco(self, preco):
+        self.preco = preco
+
+    def dados(self):
+        return f"O produto {self.nome}, custa {self.preco} reais e temos {self.qtd} unidades em estoque"
+    
+
+arroz = Produto("Arroz", 30, 45)
+feijao = Produto("Feijão", 40, 46)
+macarrao = Produto("Macarrão", 50, 47)
+bandOvo = Produto("Bandeja de ovo", 60, 48)
+cebola = Produto("Cebola", 25.00, 49)
+
+print("\nDados atuais do estoque:")
+print(arroz.dados())
+print(feijao.dados())
+print(macarrao.dados())
+print(bandOvo.dados())
+print(cebola.dados())
+
+arroz.atPreco(50)
+macarrao.atPreco(80)
+
+print("\nDados atuais do estoque:")
+print(arroz.dados())
+print(feijao.dados())
+print(macarrao.dados())
+print(bandOvo.dados())
+print(cebola.dados())
--- Questões/quest5.py
+++ Questões/quest5.py
@@ -16,3 +16,43 @@ Depois:
     Tente realizar um saque inválido.
     Mostre o saldo final de cada conta.
 '''
+
+# Aprendendo conceitos de encapsulamento.
+
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+
+        self.titular = titular
+        self._saldo = saldo
+        ''' O _ no início do nome é uma convenção em
+        Python que indica que o atributo é "interno"
+        e não deve ser acessado diretamente.'''
+
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor # Só aceita valores positivos
+
+    def sacar(self, valor):
+        if valor <= self._saldo: # Verificação de saldo
+            self._saldo -= valor # Finaliza saque e atualiza saldo
+
+        else:
+            print("\nSaldo insuficiente")
+        
+    def consultar_saldo(self):
+        return f"O saldo do cliente {self.titular} é de {self._saldo} reais"
+
+
+conta1 = ContaBancaria("Bia", 10000)
+conta2 = ContaBancaria("João", 30000)
+
+conta1.depositar(200)
+conta2.depositar(400)
+
+conta1.sacar(300)
+conta2.sacar(100)
+
+conta2.sacar(9000000)
+
+print(conta1.consultar_saldo())
+print(conta2.consultar_saldo())
