779d86f5ccdf12f3b7cf8a0a58591353a2a8b2da
Ana <ana.menezes@alu.ufc.br>
2026-07-25 17:25:53 -0300

    Resolução questão 8 completa

--- Questões/quest8.py
+++ Questões/quest8.py
@@ -1,8 +1,10 @@
 '''
-Questão 8 — Sistema de Loja: 
+Questão 8 — Sistema de Loja
+
 Implemente as classes Produto e Loja para simular o gerenciamento do estoque de um comércio.
 
 1. Atributos das Classes
+
 Produto:
     nome (string): Nome do produto.
     preco (float): Preço unitário do produto.
@@ -12,6 +14,7 @@ Loja:
     produtos (list): Lista que armazenará os objetos da classe Produto (deve iniciar vazia).
 
 2. Métodos
+
 Classe Produto:
     alterar_preco(novo_preco): Atualiza o preço do produto com o novo valor fornecido.
 
@@ -27,3 +30,62 @@ Classe Loja:
     Execute o método listar_produtos() para exibir o catálogo atualizado.
     Realize uma busca por um produto cadastrado utilizando o método procurar_produto().
 '''
+
+class Produto:
+    def __init__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+    def alterar_preco(self, novo_preco):
+        self.preco = novo_preco
+
+    def exibir(self):
+        print(f"{self.nome} - R$ {self.preco:.2f}")
+
+class Loja:
+    def __init__(self, nome_loja):
+        self.nome_loja = nome_loja
+        self.produtos = []
+
+    def cadastrar_produto(self, produto):
+        self.produtos.append(produto)
+
+    def listar_produtos(self):
+        print(f"Produtos da loja {self.nome_loja}:")
+        for produto in self.produtos:
+            produto.exibir()
+
+    def procurar_produto(self, nome):
+        for produto in self.produtos:
+            if produto.nome == nome:
+                print("Produto encontrado:")
+                produto.exibir()
+                return produto
+
+        print("Produto não existe.")
+        return None
+
+
+loja1 = Loja("Maria Bonita")
+
+arroz = Produto("Arroz", 26.00)
+feijao = Produto("Feijão", 40.00)
+macarrao = Produto("Macarrão", 8.50)
+
+loja1.cadastrar_produto(arroz)
+loja1.cadastrar_produto(feijao)
+loja1.cadastrar_produto(macarrao)
+
+print("\nListando Produtos:")
+loja1.listar_produtos()
+
+arroz.alterar_preco(30.00)
+
+print("\nListando Produtos:")
+loja1.listar_produtos()
+
+print("\nBuscando Arroz:")
+loja1.procurar_produto("Arroz")
+
+print("\nBuscando Café:")
+loja1.procurar_produto("Café")
