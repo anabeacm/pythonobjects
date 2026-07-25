08fe3890c380017769e1680f333506a371f59883
Ana <ana.menezes@alu.ufc.br>
2026-07-25 18:08:28 -0300

    Resolução questão 10 completa - Estudo e prática de encapsulamento e atributos privados

--- Questões/quest10.py
+++ Questões/quest10.py
@@ -21,3 +21,62 @@ Implemente uma classe chamada ContaBancaria utilizando conceitos de encapsulamen
     Tente acessar diretamente o atributo privado de saldo (ex: conta.__saldo) fora da classe para comprovar que o acesso está bloqueado.
     Percorra a lista contas_bancarias exibindo os dados finais de cada conta no terminal.
 '''
+
+class ContaBancaria:
+    def __init__(self, titular, numero_conta, saldo):
+        self.titular = titular
+        self.numero_conta = numero_conta
+        self.__saldo = saldo  # Atributo privado (name mangling)
+
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+        else:
+            print("Valor inválido, operação não pode ser concluída.")
+
+    def sacar(self, valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+        else:
+            print("Saldo insuficiente, operação não pode ser concluída.")
+
+    def consultar_saldo(self):
+        return f"R$ {self.__saldo:.2f}"
+
+    def exibir_dados(self):
+        print(
+            f"Titular: {self.titular} | "
+            f"Número da conta: {self.numero_conta} | "
+            f"Saldo: {self.consultar_saldo()}"
+        )
+
+contas_bancarias = []
+
+c1 = ContaBancaria("João", 1, 300.00)
+c2 = ContaBancaria("Alice", 2, 4000.99)
+
+contas_bancarias.append(c1)
+contas_bancarias.append(c2)
+
+print("\nContas cadastradas")
+for conta in contas_bancarias:
+    conta.exibir_dados()
+
+print("\nOperações")
+c1.depositar(-300.00) # print Valor inválido
+c2.depositar(500.00) # Depósito válido
+
+c1.sacar(40.00) # Saque válido
+c2.sacar(400.00) # Saque válido
+
+c2.sacar(90000.00) # print Saldo insuficiente
+
+print("\nContas após as operações")
+for conta in contas_bancarias:
+    conta.exibir_dados()
+
+print("\nTestando encapsulamento:")
+try:
+    print(c1.__saldo)
+except AttributeError:
+    print("Não é possível acessar diretamente um atributo privado.")
