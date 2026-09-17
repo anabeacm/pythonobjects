9a85eadc0291b6589402cc9259915c58ed9ddc33
Ana <ana.menezes@alu.ufc.br>
2026-09-17 16:59:47 -0300

    Anotações revisão final e ipynb formatado questão Fração

---
+++ RevisãoF.py
@@ -0,0 +1,26 @@
+# Revisão Final - Monitoria Programação Orientada a Objetos
+## Monitor Giovanni
+## 17/09/2026
+## 16h às 17:30
+
+# Em casa fazer a prova de fraçaõ para usar de exemplo
+# Revisar dunders
+
+'''
+Dunders que tarcísio usa:
+
+-- Operadores relacionais
+-- Aplicar operações lógocas
+
+Não usa bit a bit e nem digitais
+
+-- str eq len operações - soma subtração multiplicação divisão - floordiv ou truediv
+
+Sobreescreita e sobrecarga
+
+Tratamento de erros - tratamento de excessão
+
+-- Método protegido de redução da fração - filhas da fração podem usar também - aplicar em todos os peradores
+
+-- Cálculo da redução no ipynb
+'''
